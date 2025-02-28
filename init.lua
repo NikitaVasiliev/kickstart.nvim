5c8bde64d58f1a2d8604f24d759cceea27705c71
Nikita Vasiliev <Nikita.Vasiliev@virtuozzo.com>
2025-02-28 15:47:11 +0200

    Usage copilot.lua

--- init.lua
+++ init.lua
@@ -136,7 +136,7 @@ require('lazy').setup({
   {
     'CopilotC-Nvim/CopilotChat.nvim',
     dependencies = {
-      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'github/copilot.lua' }, -- or zbirenbaum/copilot.lua
       { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
     },
     build = 'make tiktoken', -- Only on MacOS or Linux
