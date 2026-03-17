4b20c180eb8a7cd32c49f4140462a4ce4d7833d7
Nikita Vasiliev <Nikita.Vasiliev@virtuozzo.com>
2026-03-17 15:00:59 +0200

    feat(plugins): Add markdown render

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-mini/mini.nvim",
+    },
+    ---@module "render-markdown"
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "markdown",
+        "markdown_inline",
+        "html",
+        "yaml",
+      })
+    end,
+  },
+}
